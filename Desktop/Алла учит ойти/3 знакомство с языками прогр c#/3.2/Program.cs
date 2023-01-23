4c0c375f9f9c1e0cdc5918598dee65da4eb7fd7a
Alla <allafaa@yandex.ru>
2023-01-23 15:54:15 +0300

    задача 1

---
+++ Desktop/Алла учит ойти/3 знакомство с языками прогр c#/3.2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+Console.Clear();
+int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+
+int b = (a / 100);
+int c = (a % 10);
+
+Console.Write(b);
+Console.Write(c);
+
+
+
