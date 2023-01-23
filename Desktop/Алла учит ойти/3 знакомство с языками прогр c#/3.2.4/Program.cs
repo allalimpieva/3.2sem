9819a57bb9b68e21d4526fb83f0ee9281c65f144
Alla <allafaa@yandex.ru>
2023-01-23 18:01:36 +0300

    задача 4

---
+++ Desktop/Алла учит ойти/3 знакомство с языками прогр c#/3.2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.Clear();
+int a = new Random().Next(8,10);
+int b = new Random().Next(0, 4);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+if ((((a * a)) == b)|| ((b * b) == a))
+{
+    Console.WriteLine("одно число является квадратом другого");
+}
+else
+{
+    Console.WriteLine("числа не являются квадратами друг друга");
+}
+
