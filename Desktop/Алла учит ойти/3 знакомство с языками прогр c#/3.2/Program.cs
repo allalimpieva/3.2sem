a15dd2ee7c283d97d6abbce2adcfc40137e304ee
Alla <allafaa@yandex.ru>
2023-01-23 16:29:26 +0300

    задача 2

--- Desktop/Алла учит ойти/3 знакомство с языками прогр c#/3.2/Program.cs
+++ Desktop/Алла учит ойти/3 знакомство с языками прогр c#/3.2/Program.cs
@@ -1,14 +1,24 @@
-﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+﻿
+/*
+ Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+то программа выводит остаток от деления.
+*/
 
 Console.Clear();
-int a = new Random().Next(100, 1000);
-Console.WriteLine(a);
 
-int b = (a / 100);
-int c = (a % 10);
+int a = new Random().Next(10, 1000);
+int b = new Random().Next(10, 1000);
+Console.WriteLine (a);
+Console.WriteLine (b);
 
-Console.Write(b);
-Console.Write(c);
+int c = (a % b);
 
-
-
+if (c == 0)
+{
+    Console.WriteLine("число 2 кратно числу 1");
+}
+else
+{
+    Console.WriteLine("число 2 не кратное, остаток " + c);
+}
